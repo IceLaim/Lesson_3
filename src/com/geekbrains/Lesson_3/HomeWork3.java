c84d62770ed9980bc87f57f6ae9ca40c0f80f3db
IceLaim <100352879+IceLaim@users.noreply.github.com>
2022-03-08 21:00:18 +0300

    Домашнее задание 3

--- src/com/geekbrains/Lesson_3/HomeWork3.java
+++ src/com/geekbrains/Lesson_3/HomeWork3.java
@@ -57,7 +57,7 @@ public class HomeWork3 {
             System.out.println();
         }
     }
-    public static int[] lenArray(int len, int initialValue){
+    public static int[] lenArray(int len, int initialValue) {
         int[] arr = new int[len];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = initialValue;

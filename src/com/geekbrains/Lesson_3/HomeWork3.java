d920fcd0f8435905bcc6b4ae5c49499b971b1f2b
IceLaim <100352879+IceLaim@users.noreply.github.com>
2022-03-08 20:49:16 +0300

    Домашнее задание 3

--- src/com/geekbrains/Lesson_3/HomeWork3.java
+++ src/com/geekbrains/Lesson_3/HomeWork3.java
@@ -10,6 +10,7 @@ public class HomeWork3 {
         fillArray();
         changeArray();
         fillMultiArray();
+        lenArray(5, 3);
 }
     public static void invertArray() {
         int[] arr = { 1, 0, 1, 0, 0, 1 };
@@ -20,15 +21,15 @@ public class HomeWork3 {
             arr[i] = 1;
             }
         }
-        System.out.print(Arrays.toString(arr));
+        System.out.print("Задание 1:" + (" ") + Arrays.toString(arr));
+        System.out.println(" ");
     }
     public static void fillArray() {
         int[] arr = new int[100];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
         }
-        System.out.println(" ");
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Задание 2:" + (" ") + Arrays.toString(arr));
     }
     public static void changeArray() {
         int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
@@ -37,16 +38,16 @@ public class HomeWork3 {
                 arr[i] = arr[i] * 2;
             }
         }
-        System.out.println(" ");
-        System.out.print(Arrays.toString(arr));
+        System.out.print("Задание 3:" + (" ") + Arrays.toString(arr));
         System.out.println(" ");
     }
 
     public static void fillMultiArray() {
         int[][] arr = new int[5][5];
-        for (int i = 0; i < 5; i++) {
+        System.out.println("Задание 4:" + (" "));
+        for (int i = 0; i < arr.length; i++) {
             int[] innerArr = arr[i];
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < arr.length; j++) {
             arr[i][arr.length - i - 1] = 1;
             if (i == j) {
                 arr[i][j] = 1;
@@ -55,5 +56,13 @@ public class HomeWork3 {
         }
             System.out.println();
         }
+    }
+    public static int[] lenArray(int len, int initialValue){
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = initialValue;
+        }
+        System.out.println("Задание 5:" + (" ") + Arrays.toString(arr));
+        return arr;
     }
 }
